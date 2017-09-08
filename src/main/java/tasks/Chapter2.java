4713da7785088a8544ab0a5dc4db4e7fe3ba7252
Ilya Artamonov <Ilya.Artamanov@consult.nordea.com>
2017-09-08 15:10:18 +0200

    Chapter 2: remove duplicates + findNthElement

--- src/main/java/tasks/Chapter2.java
+++ src/main/java/tasks/Chapter2.java
@@ -1,35 +1,104 @@
 package tasks;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class Chapter2 {
 
-    public static <T> void removeDuplicates(Node<T> t) {
-        Node<T> elem = t;
-        Map<T,Boolean> map = new HashMap();
-        while (elem.next != null) {
-            if (map.containsKey(elem.item)) {
-                elem.prev.next = elem.next;
+    public static <T> String removeDuplicates(Node<T> t) {
+        Node<T> head = t;
+        Node<T> elem = null;
+        Map<T, Boolean> map = new HashMap();
+        while (t != null) {
+            if (map.containsKey(t.data)) {
+                elem.next = t.next;
             } else {
-                map.put(elem.item, true);
+                map.put(t.data, true);
+                elem = t;
             }
-            elem = elem.next;
+            t = t.next;
         }
+        return head.toString();
     }
 
+    public static <T> String removeDuplicates3(Node<T> t) {
+        Node<T> head = t;
+        T currentData;
+        Node<T> list;
+        while (t.next != null) {
+            list = t;
+            currentData = list.data;
+            while (list.next != null) {
+                if (currentData.equals(list.next.data)) {
+                    list.next = list.next.next;
+                } else {
+                    list = list.next;
+                }
+            }
+            if (t.next != null) {
+                t = t.next;
+            }
+        }
+        return head.toString();
+    }
+
+    public static <T> T findNthElement(Node<T> head, int n) throws Exception {
+        Node<T> elem = head;
+        int linkedListLength = 1;
+        while(elem.next != null) {
+            linkedListLength++;
+            elem = elem.next;
+        }
+        if (linkedListLength-1 < n) {
+            throw new Exception("Not enough elements in list");
+        }
+        int needToFind = linkedListLength - n;
+        elem = head;
+
+        if (needToFind == 0) {
+            return elem.data;
+        }
+        for(int i = 1; i < needToFind; i++) {
+            elem = elem.next;
+        }
+        return elem.next.data;
+    }
+
+    public static <T> T findNthElement2(Node<T> head, int n) throws Exception {
+        if (n < 1) {
+            throw new Exception("Not enough elements in list");
+        }
+        Node<T> p1 = head;
+        Node<T> p2 = head;
+        for (int i = 0; i < n - 1; i++) {
+            if (p1 == null) {
+                throw new Exception("Not enough elements in list");
+            }
+            p1 = p1.next;
+        }
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2.data;
+    }
+
+
+
+
+
+
     public static class Node<E> {
-        E item;
+        E data;
         Node<E> next;
         Node<E> prev;
 
         Node(E data) {
-            this.item = data;
+            this.data = data;
         }
 
         Node(Node<E> prev, E element, Node<E> next) {
-            this.item = element;
+            this.data = element;
             this.next = next;
             this.prev = prev;
         }
@@ -45,13 +114,15 @@ public class Chapter2 {
             return this;
         }
 
-        public String print() {
+        @Override
+        public String toString() {
             StringBuilder result = new StringBuilder();
             Node n = this;
             while (n.next != null) {
-                result.append(n.item);
+                result.append(n.data);
                 n = n.next;
             }
+            result.append(n.data);
             return result.toString();
         }
 
--- src/test/java/tasks/TestChapter2.java
+++ src/test/java/tasks/TestChapter2.java
@@ -1,22 +1,39 @@
 package tasks;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static helpers.Helper.createMatrix;
 
 public class TestChapter2 {
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Test
     public void removeDuplicatesTest() {
         Chapter2.Node n = new Chapter2.Node(1).add(2).add(3).add(4).add(2).add(5);
-        System.out.println(n.print());
-        Chapter2.removeDuplicates(n);
-        System.out.println(n.print());
-        Assert.assertEquals("12345",n.print());
+        Assert.assertEquals("12345",Chapter2.removeDuplicates(n));
 
+        Chapter2.Node n2 = new Chapter2.Node(1).add(1).add(1).add(2).add(2).add(2);
+        Assert.assertEquals("12",Chapter2.removeDuplicates(n2));
 
+        Chapter2.Node n3 = new Chapter2.Node(1).add(1).add(1).add(1).add(1).add(1);
+        Assert.assertEquals("1",Chapter2.removeDuplicates(n3));
     }
 
+    @Test
+    public void findNthElementTest() throws Exception {
+        Chapter2.Node n = new Chapter2.Node(1).add(2).add(3).add(4).add(5).add(6).add(7).add(8);
+        Assert.assertEquals(8,Chapter2.findNthElement(n, 1));
+        Assert.assertEquals(2,Chapter2.findNthElement(n, 7));
+        n = new Chapter2.Node(1).add(2);
+        Assert.assertEquals(2,Chapter2.findNthElement(n, 1));
+
+        exception.expect(Exception.class);
+        Assert.assertEquals(2,Chapter2.findNthElement(n, 3));
+    }
 }
 
